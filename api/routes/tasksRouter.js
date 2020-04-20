77d8aaf273da014af47c516f1806499dca4a08f3
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-04-20 12:35:03 -0400

    All routes implemented

---
+++ api/routes/tasksRouter.js
@@ -0,0 +1,63 @@
+const express = require("express");
+
+const Tasks = require("../helpers/tasksModel");
+
+const db = require("../../data/db-config");
+
+const { validateId, validateTask } = require("../middleware/middleware");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+	Tasks.find()
+		.then(tasks => res.status(200).json(tasks))
+		.catch(error =>
+			res.status(500).json({ error: "Failed to get tasks", error })
+		);
+});
+
+router.get("/:id", validateId(db, "tasks"), (req, res) => {
+	res.status(200).json(req.response);
+});
+
+router.post("/", validateTask, (req, res) => {
+	Tasks.add(req.response)
+		.then(task => res.status(201).json(task))
+		.catch(error =>
+			res.status(500).json({ error: "Failed to add task", error })
+		);
+});
+
+router.put("/:id", validateId(db, "tasks"), validateTask, (req, res) => {
+	const { id } = req.params;
+
+	Tasks.update(id, req.response)
+		.then(count => {
+			count
+				? res.status(200).json({ message: "Task updated successfully" })
+				: res
+						.status(400)
+						.json({ error: `Could not update task with id ${id}` });
+		})
+		.catch(error =>
+			res.status(500).json({ message: "Error updating the task", error })
+		);
+});
+
+router.delete("/:id", validateId(db, "tasks"), (req, res) => {
+	const { id } = req.params;
+
+	Tasks.remove(id)
+		.then(count =>
+			count
+				? res.status(200).json({ message: "Task deleted successfully" })
+				: res
+						.status(400)
+						.json({ message: `Could not delete task with id ${id}` })
+		)
+		.catch(error =>
+			res.status(500).json({ message: "Error deleting the task", error })
+		);
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const helmet = require("helmet");
 
 const resourcesRouter = require("./routes/resourcesRouter");
 const projectsRouter = require("./routes/projectsRouter");
+const tasksRouter = require("./routes/tasksRouter");
 
 const server = express();
 
@@ -10,6 +11,7 @@ server.use(express.json());
 server.use(helmet());
 server.use("/api/resources", resourcesRouter);
 server.use("/api/projects", projectsRouter);
+server.use("/api/tasks", tasksRouter);
 
 server.get("/", (req, res) => {
 	res.status(200).json({ Api: "up" });
